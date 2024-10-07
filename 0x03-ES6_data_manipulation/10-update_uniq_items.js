89a7ead06643e00eb88874d718e3181fbc5f42d9
Ilayoub <jrindou.ayoub@gmail.com>
2024-10-07 22:53:53 +0000

    More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,13 @@
-export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
-  }
+const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+
   map.forEach((value, key) => {
     if (value === 1) {
       map.set(key, 100);
     }
   });
-}
+
+  return map;
+};
+
+export default updateUniqueItems;
