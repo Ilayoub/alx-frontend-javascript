4c606e4cfc074f0dcb004ac7561ed98c95113125
Ilayoub <jrindou.ayoub@gmail.com>
2024-10-07 19:40:06 +0000

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,16 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-
+  // Use filter to get students from specified city
   const studentByCity = studentList.filter((student) => student.location === city);
 
+  // Use the map to make update for grades
   const updatedStudents = studentByCity.map((student) => {
-
+    // Find the grade object for the student in new Grades array
     const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
 
+    // If the grade unfound, update else set N/A
     const updatedGrade = gradeObject ? gradeObject.grade : 'N/A';
 
+    // Return a new object with updated grade
     return {
       ...student,
       grade: updatedGrade,
